52b53872b65727425aa242fa2b69b4500e492742
dr-js <dr@dr.run>
2022-01-13 03:13:53 +0000

    DEL: drop legacy pack files

--- firebase-admin-verify-id-token/source/index.js
+++
@@ -1,7 +0,0 @@
-import { FirebaseTokenGenerator } from 'firebase-admin/lib/auth/token-generator' // TODO: direct import will result in a much smaller pack
-import { Certificate } from 'firebase-admin/lib/auth/credential' // TODO: direct import will result in a much smaller pack
-
-export {
-  FirebaseTokenGenerator,
-  Certificate
-}
