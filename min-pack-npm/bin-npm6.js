775aeba3f2f0f1bbd001aaf418bfe67f63b75d72
dr-js <dr@dr.run>
2022-02-09 02:11:59 +0000

    CHG: npm: reduce network request

--- min-pack-npm/bin-npm6.js
+++ min-pack-npm/bin-npm6.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
+process.env.npm_config_audit = 'false' // https://docs.npmjs.com/cli/v6/using-npm/config#audit
+process.env.npm_config_fund = 'false' // https://docs.npmjs.com/cli/v6/using-npm/config#fund
 process.env.npm_config_update_notifier = 'false' // https://docs.npmjs.com/cli/v6/using-npm/config#update-notifier
 require('./node_modules/npm6/bin/npm-cli.js')
--- min-pack-npm/bin-npm8.js
+++ min-pack-npm/bin-npm8.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 process.env.npm_config_lockfile_version = '3' // https://docs.npmjs.com/cli/v8/using-npm/config#lockfile-version
+process.env.npm_config_audit = 'false' // https://docs.npmjs.com/cli/v8/using-npm/config#audit
+process.env.npm_config_fund = 'false' // https://docs.npmjs.com/cli/v8/using-npm/config#fund
 process.env.npm_config_update_notifier = 'false' // https://docs.npmjs.com/cli/v8/using-npm/config#update-notifier
 require('./node_modules/npm8/bin/npm-cli.js')
