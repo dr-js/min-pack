36540d45c30e8d0ae4cae4758aeb4f1cb117226b
dr-js <dr@dr.run>
2022-05-18 03:57:28 +0000

    VERSION: `@min-pack/core-js-modern@0.0.3`

--- min-pack-core-js-modern/bundle-core-js-modern.js
+++ min-pack-core-js-modern/bundle-core-js-modern.js
@@ -8,17 +8,26 @@ const compat = require('core-js-compat')
 const { config: { CORE_JS_TARGET } } = require('./package.json')
 
 runKit(async (kit) => {
+  const CORE_JS_CONFIG = {
+    targets: CORE_JS_TARGET,
+    exclude: [
+      'core-js/stage/3', 'core-js/stage/2', 'core-js/stage/1', 'core-js/stage/0',
+      'core-js/stage/pre',
+      /^web\./
+    ]
+  }
+
   const {
     list, // array of required modules
     targets // object with targets for each module
-  } = compat({ targets: CORE_JS_TARGET })
+  } = compat(CORE_JS_CONFIG)
   // console.log(`list[${list.length}]:`, list)
   console.log(`targets[${Object.keys(targets).length}]:`, targets)
-  writeJSONPrettySync(kit.fromOutput('compat.json'), { list, targets })
+  writeJSONPrettySync(kit.fromOutput('compat.json'), { list })
 
   const filename = kit.fromOutput('bundle.js')
   await builder({
-    filename, targets: CORE_JS_TARGET,
+    filename, ...CORE_JS_CONFIG,
     summary: { // shows summary for the bundle, disabled by default:
       console: { size: true, modules: true }, // in the console, you could specify required parts or set `true` for enable all of them
       comment: false // in the comment in the target file, similarly to `summary.console`
