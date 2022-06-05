4c16efdc4cf3f15599cf54c13ffa3cfa464059b2
dr-js <dr@dr.run>
2022-06-05 06:08:58 +0000

    VERSION: `@min-pack/core-js-modern@0.0.4`

--- min-pack-core-js-modern/build.js
+++ min-pack-core-js-modern/build.js
@@ -1,10 +1,14 @@
 const { runKit } = require('@dr-js/core/library/node/kit.js')
-const { modifyCopy } = require('@dr-js/core/library/node/fs/Modify.js')
+const { modifyCopy, modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
 const { editPackageJSON } = require('@dr-js/core/library/node/module/PackageJSON.js')
 
 const { initOutput } = require('@dr-js/dev/library/output.js')
 
 runKit(async (kit) => {
+  await modifyDeleteForce(kit.fromRoot('node_modules/'))
+  await modifyDeleteForce(kit.fromRoot('package-lock.json'))
+  kit.RUN('npm install --lockfile-version 3 --no-audit --no-fund')
+
   await initOutput({ kit })
   await modifyCopy(kit.fromRoot('node_modules/core-js-compat/LICENSE'), kit.fromOutput('LICENSE'))
   await editPackageJSON((packageJSON) => {
@@ -20,6 +24,5 @@ runKit(async (kit) => {
     return packageJSON
   }, kit.fromOutput('package.json'))
 
-  kit.RUN('npm install')
   kit.RUN('npm run bundle-core-js-modern')
-}, { title: 'fetch-latest-ssh2' })
+}, { title: 'build-core-js-modern' })
