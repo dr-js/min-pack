6603bcae77526baa6f5e55c8f926cdb57dde2864
dr-js <dr@dr.run>
2023-08-24 12:07:57 +0000

    ADD: `@min-pack/core-js-legacy@0.1.0`
    core-js-builder@3.32.1 core-js-compat@3.32.1

---
+++ min-pack-core-js-legacy/README.md
@@ -0,0 +1,3 @@
+# @min-pack/core-js-legacy
+
+Custom build ['core-js-bundle'](https://github.com/zloirock/core-js/tree/v3.22.5#custom-build) targets: 'chrome 45, safari 11, firefox 52, edge 14'
---
+++ min-pack-core-js-legacy/build.js
@@ -0,0 +1,28 @@
+const { runKit } = require('@dr-js/core/library/node/kit.js')
+const { modifyCopy, modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
+const { editPackageJSON } = require('@dr-js/core/library/node/module/PackageJSON.js')
+
+const { initOutput } = require('@dr-js/dev/library/output.js')
+
+runKit(async (kit) => {
+  await modifyDeleteForce(kit.fromRoot('node_modules/'))
+  await modifyDeleteForce(kit.fromRoot('package-lock.json'))
+  kit.RUN('npm install --lockfile-version 3 --no-audit --no-fund --no-update-notifier')
+
+  await initOutput({ kit })
+  await modifyCopy(kit.fromRoot('node_modules/core-js-compat/LICENSE'), kit.fromOutput('LICENSE'))
+  await editPackageJSON((packageJSON) => {
+    const builderPackageJSON = require('core-js-builder/package.json')
+    const compatPackageJSON = require('core-js-compat/package.json')
+
+    // packageJSON[ 'version' ] = builderPackageJSON.version
+    packageJSON[ 'license' ] = builderPackageJSON.license
+    packageJSON[ 'config' ][ 'CORE_JS_VERSION' ] = [
+      `core-js-builder@${builderPackageJSON.version}`,
+      `core-js-compat@${compatPackageJSON.version}`
+    ].join(' ')
+    return packageJSON
+  }, kit.fromOutput('package.json'))
+
+  kit.RUN('npm run bundle-core-js-legacy')
+}, { title: 'build-core-js-legacy' })
