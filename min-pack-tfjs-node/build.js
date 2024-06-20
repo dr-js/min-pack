e2537a4bf6bbad533b836cb33531bdddeb7b6698
dr-js <dr@dr.run>
2024-06-20 03:31:11 +0000

    CHG: tfjs-node: use v2.17.2 & update script

--- min-pack-tfjs-node/build.js
+++ min-pack-tfjs-node/build.js
@@ -1,6 +1,5 @@
 const { runKit } = require('@dr-js/core/library/node/kit.js')
-const { modifyCopy, modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
-const { editPackageJSON } = require('@dr-js/core/library/node/module/PackageJSON.js')
+const { modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
 const { initOutput } = require('@dr-js/dev/library/output.js')
 
 runKit(async (kit) => {
@@ -9,17 +8,16 @@ runKit(async (kit) => {
   await modifyDeleteForce(kit.fromRoot('package-lock.json'))
   kit.RUN('npm install --lockfile-version 3 --no-audit --no-fund --no-update-notifier')
 
-  await initOutput({ kit })
-  await modifyCopy(kit.fromRoot('node_modules/@xenova/transformers/LICENSE'), kit.fromOutput('LICENSE'))
-
-  await editPackageJSON((packageJSON) => {
-    packageJSON[ 'overrides' ] = undefined
-    const pkgPackageJSON = require('@xenova/transformers/package.json')
-    packageJSON[ 'version' ] ||= pkgPackageJSON.version
-    packageJSON[ 'license' ] = pkgPackageJSON.license
-    packageJSON[ 'config' ][ 'TRANSFORMERS_JS_VERSION' ] = pkgPackageJSON.version
-    return packageJSON
-  }, kit.fromOutput('package.json'))
+  await initOutput({
+    kit, extraDeleteKeyList: [ 'overrides' ],
+    copyMapPathList: [ [ 'node_modules/@xenova/transformers/LICENSE', 'LICENSE' ] ],
+    editPackageJSON: (packageJSON) => {
+      const pkgPackageJSON = require('@xenova/transformers/package.json')
+      packageJSON[ 'version' ] ||= pkgPackageJSON.version
+      packageJSON[ 'license' ] = pkgPackageJSON.license
+      packageJSON[ 'config' ][ 'TRANSFORMERS_JS_VERSION' ] = pkgPackageJSON.version
+    }
+  })
 
   kit.RUN('npm run build-webpack')
 }, { title: 'build-tfjs-node' })
