ff2b3139a09edc086c9f77bfb308a93c21c2a47b
dr-js <dr@dr.run>
2024-06-20 03:45:28 +0000

    CHG: tfjs-node: lancedb v0.5.2 & update script

--- min-pack-lancedb/build.js
+++ min-pack-lancedb/build.js
@@ -1,5 +1,5 @@
 const { runKit } = require('@dr-js/core/library/node/kit.js')
-const { /* modifyCopy, */ modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
+const { modifyDeleteForce } = require('@dr-js/core/library/node/fs/Modify.js')
 const { editPackageJSON } = require('@dr-js/core/library/node/module/PackageJSON.js')
 const { initOutput } = require('@dr-js/dev/library/output.js')
 const { fetchNpmPkg } = require('../function.js')
@@ -10,6 +10,9 @@ runKit(async (kit) => {
   await modifyDeleteForce(kit.fromRoot('package-lock.json'))
   kit.RUN('npm install --lockfile-version 3 --no-audit --no-fund --no-update-notifier')
 
+  // NOTE: since 0.5, export is limited to index files
+  await editPackageJSON((packageJSON) => ({ ...packageJSON, exports: undefined }), kit.fromRoot('node_modules/@lancedb/lancedb/package.json'))
+
   const { version: pkgVersion } = require('@lancedb/lancedb/package.json')
   kit.padLog(`force install linux x64/arm64 addon @${pkgVersion}`)
   await modifyDeleteForce(kit.fromRoot('node_modules/@lancedb/lancedb-linux-arm64-gnu/'))
@@ -17,17 +20,15 @@ runKit(async (kit) => {
   await fetchNpmPkg(kit, '@lancedb/lancedb-linux-arm64-gnu', pkgVersion, kit.fromRoot('node_modules/@lancedb/lancedb-linux-arm64-gnu/'))
   await fetchNpmPkg(kit, '@lancedb/lancedb-linux-x64-gnu', pkgVersion, kit.fromRoot('node_modules/@lancedb/lancedb-linux-x64-gnu/'))
 
-  await initOutput({ kit })
-  // await modifyCopy(kit.fromRoot('node_modules/@lancedb/lancedb/LICENSE'), kit.fromOutput('LICENSE'))
-
-  await editPackageJSON((packageJSON) => {
-    packageJSON[ 'overrides' ] = undefined
-    const pkgPackageJSON = require('@lancedb/lancedb/package.json')
-    packageJSON[ 'version' ] ||= pkgPackageJSON.version
-    packageJSON[ 'license' ] = pkgPackageJSON.license
-    packageJSON[ 'config' ][ 'LANCEDB_VERSION' ] = pkgPackageJSON.version
-    return packageJSON
-  }, kit.fromOutput('package.json'))
+  await initOutput({
+    kit, extraDeleteKeyList: [ 'overrides' ],
+    editPackageJSON: (packageJSON) => {
+      const pkgPackageJSON = require('@lancedb/lancedb/package.json')
+      packageJSON[ 'version' ] ||= pkgPackageJSON.version
+      packageJSON[ 'license' ] = pkgPackageJSON.license
+      packageJSON[ 'config' ][ 'LANCEDB_VERSION' ] = pkgPackageJSON.version
+    }
+  })
 
   kit.RUN('npm run build-webpack')
 }, { title: 'build-lancedb' })
