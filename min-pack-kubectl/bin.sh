5eeaf49a66706abb37629ea7a87cd88904230df8
dr-js <dr@dr.run>
2026-05-29 07:04:26 +0000

    VER: `@min-pack/kubectl@0.1.0`
    v1.36.1

--- min-pack-kubectl/bin.sh
+++ min-pack-kubectl/bin.sh
@@ -4,7 +4,13 @@ OS="$(uname -s)"
 ARCH="$(uname -m)"
 DIR="$(dirname "$(realpath "$0")")"
 
-if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then exec "$DIR/../kubectl-linux-x64/kubectl" "$@"
-elif [[ "$OS" == "Linux" && "$ARCH" == "aarch64" ]]; then exec "$DIR/../kubectl-linux-arm64/kubectl" "$@"
+if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
+  if [[ -f "$DIR/../kubectl-linux-x64/kubectl" ]]; then exec "$DIR/../kubectl-linux-x64/kubectl" "$@"
+  else exec "$DIR/node_modules/@min-pack/kubectl-linux-x64/kubectl" "$@"
+  fi
+elif [[ "$OS" == "Linux" && "$ARCH" == "aarch64" ]]; then
+  if [[ -f "$DIR/../kubectl-linux-arm64/kubectl" ]]; then exec "$DIR/../kubectl-linux-arm64/kubectl" "$@"
+  else exec "$DIR/node_modules/@min-pack/kubectl-linux-arm64/kubectl" "$@"
+  fi
 else echo "no binary for $OS-$ARCH" >&2; exit 1
 fi
