3a5ee8eabbd76d39f7b3fb331f1badd501cd65d9
dr-js <dr@dr.run>
2026-07-13 01:41:12 +0000

    CHG: sort `bin.sh`

--- min-pack-kubectl/bin.sh
+++ min-pack-kubectl/bin.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
+set -e
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 DIR="$(dirname "$(realpath "$0")")"
-
 if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
   if [[ -f "$DIR/../kubectl-linux-x64/kubectl" ]]; then exec "$DIR/../kubectl-linux-x64/kubectl" "$@"
   else exec "$DIR/node_modules/@min-pack/kubectl-linux-x64/kubectl" "$@"
--- min-pack-ss-rust/bin.sh
+++ min-pack-ss-rust/bin.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
+set -e
 OS="$(uname -s)"
 ARCH="$(uname -m)"
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
+DIR="$(dirname "$(realpath "$0")")"
 if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then exec "$DIR/ssservice-linux-x86_64" "$@"
 elif [[ "$OS" == "Linux" && "$ARCH" == "aarch64" ]]; then exec "$DIR/ssservice-linux-aarch64" "$@"
 else echo "no binary for $OS-$ARCH" >&2; exit 1
